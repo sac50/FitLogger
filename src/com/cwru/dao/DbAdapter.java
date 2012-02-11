0316efe24ad9f7693ed3fbfe23a67ba3a087e67f
Lowell Kissling <lxk82@case.edu>
2012-02-11 15:30:27 -0500

    initial versions of DbAdapter, Exercise and Set

---
+++ src/com/cwru/dao/DbAdapter.java
@@ -0,0 +1,145 @@
+package com.cwru.dao;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.cwru.model.Exercise;
+import com.cwru.model.Set;
+
+public class DbAdapter {
+	private DatabaseHelper dbHelper;
+	private SQLiteDatabase db;
+	private static final String TAG = "dbHelper";
+	
+	
+	
+	private static final String DATABASE_CREATE =
+	        "create table workouts (_id integer primary key autoincrement, "
+	        + "name text not null, type text not null, exercise_sequence "
+	        + "text not null, coment text, repeatable boolean not null);"
+	        + "create table exercises (_id integer primary key autoincrement, "
+	        + "name text not null, type text not null, sets integer, "
+	        + "time integer, is_countdown boolean, distance real,"
+	        + "distance_type text, interval integer, comment text, boolean deleted not null);"
+	        + "create table sets (_id integer primary key autoincrement, "
+	        + "exercise_id integer not null, reps integer not null, "
+	        + "weight real not null);"
+	        + "create table workout_results (_id integer primary key autoincrement, "
+	        + "date text not null, workout_id integer not null, exercise_id "
+	        + "integer not null, sets integer, reps integer, weight real, "
+	        + "time integer, time_type boolean, distance real, interval"
+	        + "integer, comment text);";
+	
+	private static final String DATABASE_NAME = "FitLogger Data";
+	private static final String DATABASE_TABLE_WORKOUT = "workouts";
+	private static final String DATABASE_TABLE_EXERCISE = "exercises";
+	private static final String DATABASE_TABLE_SET = "sets";
+	private static final String DATABASE_TABLE_WORKOUT_RESULT = "workout_results";
+	private static final int DATABASE_VERSION = 1;
+	
+	private final Context mCtx;
+	
+	private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL(DATABASE_CREATE);
+        }
+        
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS notes");
+            onCreate(db);
+        }
+	}
+	
+	/**
+     * Constructor - takes the context to allow the database to be
+     * opened/created
+     * 
+     * @param ctx the Context within which to work
+     */
+    public DbAdapter(Context ctx) {
+        this.mCtx = ctx;
+    }
+    
+    public DbAdapter open() throws SQLException {
+        dbHelper = new DatabaseHelper(mCtx);
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+    
+    public long createExercise(Exercise ex) {
+    	ContentValues initialValues = new ContentValues();
+    	initialValues.put("name", ex.getName());
+    	initialValues.put("type", ex.getType());
+    	
+    	if (ex.getSets() > 0) {
+        	initialValues.put("sets", ex.getSets());
+    	}
+    	
+    	if (!ex.getIsCountdown()) {
+    		initialValues.put("is_countdown", ex.getIsCountdown());
+    	} else if (ex.getIsCountdown() && ex.getTime() > 0) {
+    		initialValues.put("time", ex.getTime());
+    		initialValues.put("is_countdown", ex.getIsCountdown());
+    	}
+    	
+    	if (ex.getDistance() > 0) {
+    		initialValues.put("distance", ex.getDistance());
+        	initialValues.put("distance_type", ex.getDistanceType());
+    	}
+    	
+    	initialValues.put("comment", ex.getComment() != null ? ex.getComment() : null);
+    	initialValues.put("deleted", false);
+    	
+    	return db.insert(DATABASE_TABLE_EXERCISE, null, initialValues);
+    }
+    
+    public boolean deleteExercise(long exID) {
+    	ContentValues args = new ContentValues();
+    	args.put("deleted", true);
+    	
+    	return db.update(DATABASE_TABLE_EXERCISE, args, "id = " + exID, null) > 0;
+    }
+    
+    public long createSet(Set set) {
+    	ContentValues initialValues = new ContentValues();
+    	initialValues.put("exercise_id", set.getExerciseId());
+    	initialValues.put("reps", set.getReps());
+    	initialValues.put("weight", set.getWeight());
+    	
+    	return db.insert(DATABASE_TABLE_SET, null, initialValues);
+    }
+    
+    public boolean deleteSet(long setID) {
+    	return db.delete(DATABASE_TABLE_SET, "id = " + setID, null) > 0;
+    }
+    
+    public boolean updateSet(Set set) {
+    	ContentValues args = new ContentValues();
+    	args.put("exercise_id", set.getExerciseId());
+    	args.put("reps", set.getReps());
+    	args.put("weight", set.getWeight());
+    	
+    	return db.update(DATABASE_TABLE_SET, args, "id = " + set.getId(), null) > 0;
+    }    
+    
+}
