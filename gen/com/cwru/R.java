802ec2ae5be39108b8553ac36de13a0284bc4af5
Lowell Kissling <lxk82@case.edu>
2012-02-13 17:07:48 -0500

    added ExerciseName and ExerciseType inputs to CreateExercise tab

--- gen/com/cwru/R.java
+++ gen/com/cwru/R.java
@@ -9,7 +9,8 @@ package com.cwru;
 
 public final class R {
     public static final class array {
-        public static final int exerciseBank=0x7f050002;
+        public static final int exerciseBank=0x7f050003;
+        public static final int exersiseTypes=0x7f050002;
         public static final int modules=0x7f050000;
         public static final int workoutTypes=0x7f050001;
     }
@@ -19,21 +20,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int FLcreateWorkoutBorder1=0x7f060006;
-        public static final int FLcreateWorkoutBorder2=0x7f060008;
-        public static final int FLcreateWorkoutTabLeftFrame=0x7f060005;
-        public static final int FLcreateWorkoutTabMiddleFrame=0x7f060007;
-        public static final int FLcreateWorkoutTabRightFrame=0x7f060009;
-        public static final int LLcreateWorkoutTabContainer=0x7f060004;
-        public static final int cbEBExerciseCheckbox=0x7f06000c;
-        public static final int etCreateWorkoutName=0x7f060001;
-        public static final int spnWorkoutType=0x7f060003;
-        public static final int tvCreateWorkoutType=0x7f060002;
-        public static final int tvCreateWorkoutWorkoutName=0x7f060000;
-        public static final int tvEBExerciseLabel=0x7f06000b;
-        public static final int tvExerciseBank=0x7f06000a;
-        public static final int tvExerciseSequence=0x7f06000d;
-        public static final int tvTitle=0x7f06000e;
+        public static final int FLcreateWorkoutBorder1=0x7f06000b;
+        public static final int FLcreateWorkoutBorder2=0x7f06000d;
+        public static final int FLcreateWorkoutTabLeftFrame=0x7f06000a;
+        public static final int FLcreateWorkoutTabMiddleFrame=0x7f06000c;
+        public static final int FLcreateWorkoutTabRightFrame=0x7f06000e;
+        public static final int FLmainFrame=0x7f060009;
+        public static final int LLcreateWorkoutTabContainer=0x7f060008;
+        public static final int cbEBExerciseCheckbox=0x7f060011;
+        public static final int etCreateExerciseName=0x7f060001;
+        public static final int etCreateWorkoutName=0x7f060005;
+        public static final int spnCreateExerciseType=0x7f060003;
+        public static final int spnWorkoutType=0x7f060007;
+        public static final int tvCreateExerciseExerciseName=0x7f060000;
+        public static final int tvCreateExerciseExerciseType=0x7f060002;
+        public static final int tvCreateWorkoutType=0x7f060006;
+        public static final int tvCreateWorkoutWorkoutName=0x7f060004;
+        public static final int tvEBExerciseLabel=0x7f060010;
+        public static final int tvExerciseBank=0x7f06000f;
+        public static final int tvExerciseSequence=0x7f060012;
+        public static final int tvTitle=0x7f060013;
     }
     public static final class layout {
         public static final int create_exercise_tab=0x7f030000;
@@ -50,6 +56,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int exerciseName=0x7f040004;
+        public static final int exerciseType=0x7f040005;
         public static final int hello=0x7f040000;
         public static final int workoutName=0x7f040002;
         public static final int workoutType=0x7f040003;
--- src/com/cwru/controller/CreateExerciseActivity.java
+++ src/com/cwru/controller/CreateExerciseActivity.java
@@ -1,7 +1,11 @@
 package com.cwru.controller;
 
+import com.cwru.R;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class CreateExerciseActivity extends Activity {
@@ -11,6 +15,12 @@ public class CreateExerciseActivity extends Activity {
 		
 		TextView textView = new TextView(this);
 		textView.setText("Create Exercise Tab");
-		setContentView(textView);
+		setContentView(R.layout.create_exercise_tab);
+		
+		Spinner typeSpinner = (Spinner) findViewById(R.id.spnCreateExerciseType);
+		ArrayAdapter<CharSequence> exerTypeAdapter = ArrayAdapter.createFromResource(this,
+				R.array.exersiseTypes, android.R.layout.simple_spinner_item);
+		exerTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		typeSpinner.setAdapter(exerTypeAdapter);
 	}
 }
