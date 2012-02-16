c467eb9a48c2a8b4f2e607ed6a85da74f06cd694
Lowell Kissling <lxk82@case.edu>
2012-02-15 19:31:01 -0500

    Added dynamic set creation/deletion, removed out-of-date files

--- src/com/cwru/controller/CreateExerciseActivity.java
+++ src/com/cwru/controller/CreateExerciseActivity.java
@@ -1,23 +1,31 @@
 package com.cwru.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.cwru.R;
 import com.cwru.dao.DbAdapter;
 import com.cwru.model.Exercise;
+import com.cwru.model.SetSpinnerFragment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class CreateExerciseActivity extends Activity {
+public class CreateExerciseActivity extends FragmentActivity {
 	DbAdapter mDbHelper;
 	EditText mNameText;
 	String exType;
+	List<LinearLayout> inflatedSets = new ArrayList<LinearLayout>();
 	
 	
 	
@@ -49,7 +57,7 @@ public class CreateExerciseActivity extends Activity {
 			      // Do nothing.
 			    }
 		});
-		/*
+		
 		Spinner subTypeSpinner = (Spinner) findViewById(R.id.spnCreateExerciseSubType);
 		ArrayAdapter<CharSequence> exerSubTypeAdapter = ArrayAdapter.createFromResource(this,
 				R.array.exerciseSubTypes, android.R.layout.simple_spinner_item);
@@ -60,7 +68,51 @@ public class CreateExerciseActivity extends Activity {
 			@Override
 			public void onItemSelected(AdapterView<?> parent,
 					View view, int pos, long id) {
+				final LinearLayout setLayout = (LinearLayout) findViewById(R.id.llCreateExerciseSets);
+//				inflatedSets = new ArrayList<LinearLayout>();
 				
+				if ("Set-based".equals(parent.getItemAtPosition(pos).toString())) {
+					setLayout.setVisibility(0);
+					
+					for (int i = 0; i < 3; i++) {
+						LinearLayout inflatedSet;
+						inflatedSet = (LinearLayout) View.inflate(parent.getContext(), R.layout.create_exercise_set_builder, null);
+						inflatedSets.add(inflatedSet);
+						setLayout.addView(inflatedSet);
+					}
+					
+					Button addSet = (Button) findViewById(R.id.btnCreateExerciseAddSet);
+					addSet.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							if (inflatedSets.size() < 10) {
+								LinearLayout inflatedSet;
+								inflatedSet = (LinearLayout) View.inflate(v.getContext(), R.layout.create_exercise_set_builder, null);
+								inflatedSets.add(inflatedSet);
+								setLayout.addView(inflatedSet);
+							}
+						}
+					});
+					
+					Button removeSet = (Button) findViewById(R.id.btnCreateExerciseRemoveSet);
+					removeSet.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							if (inflatedSets.size() > 1) {
+								int position = inflatedSets.size() - 1;
+								setLayout.removeView(inflatedSets.get(position));
+								inflatedSets.remove(position);
+							}
+						}
+					});
+				} else {
+					while (!inflatedSets.isEmpty()) {
+						int position = inflatedSets.size() - 1;
+						setLayout.removeView(inflatedSets.get(position));
+						inflatedSets.remove(position);
+					}
+					setLayout.setVisibility(8);
+				}
 			}
 			
 			@Override
@@ -68,7 +120,7 @@ public class CreateExerciseActivity extends Activity {
 				
 			}
 		});
-		*/
+		
 		Button doneButton = (Button) findViewById(R.id.btnCreateExerciseDone);
 		doneButton.setOnClickListener(new View.OnClickListener() {
 			
