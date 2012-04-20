28cc01eaf36c0cac1f016714398fb44252b99eec
Steven Crilley <steven.crilley@gmail.com>
2012-04-20 11:41:37 -0400

    Commented the Workout Workflow.java file

--- src/com/cwru/controller/WorkoutWorkflow.java
+++ src/com/cwru/controller/WorkoutWorkflow.java
@@ -11,28 +11,43 @@ import android.widget.TextView;
 import com.cwru.R;
 import com.cwru.fragments.WorkoutListingFragment;
 import com.cwru.fragments.WorkoutListingFragment.onWorkoutListingClickListener;
-
+/**
+ * Activity that holds the Workout workflow module.  This module allows
+ * the user to execute a workout and record the data from it.  
+ * @author sacrilley
+ *
+ */
 public class WorkoutWorkflow extends FragmentActivity implements onWorkoutListingClickListener {	
+	/** TextView for the Application Title Bar that when clicked takes user to the Home Screenn */
 	private TextView appTitleBar;
 	
 	@Override
+	/** 
+	 * onCreate Callback for Activity.
+	 * Sets the Layout and loads the WorkoutListingFragment into the forefront.
+	 */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.workout_workflow);
 		// Set Fragment Listeners
 		WorkoutListingFragment.setOnWorkoutListingClickListener(this);
 		
+		// Get Title Bar from Layout
 		appTitleBar = (TextView) this.findViewById(R.id.tvWorkoutWorkflowAppTitleBar);
+		// Set listener to send user to home page when text view clicked
 		appTitleBar.setOnClickListener(goHomeListener);
 		
+		// Load WorkoutListing Fragment into the forefront
 		WorkoutListingFragment workouts = new WorkoutListingFragment();
 		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 		transaction.replace(R.id.flWorkoutWorkflowMainFrame, workouts);
 		transaction.commit();
-		Log.d("Steve", "Workout onCreate call");
-		
 	}
 	
+	/**
+	 * Create intent to take user to the Home Screen.  The intent resets the application 
+	 * back stack on click.
+	 */
 	View.OnClickListener goHomeListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
@@ -43,11 +58,13 @@ public class WorkoutWorkflow extends FragmentActivity implements onWorkoutListin
 	};
 
 	@Override
+	/**
+	 * Listener that when a workout list entry is clicked, it takes the user to the screen
+	 * to start performing the workout and the exercises belonging to it
+	 */
 	public void onWorkoutListingListenerClick(String workoutName) {
 		Intent intent = new Intent(this, PerformWorkout.class);
 		intent.putExtra("workoutName", workoutName);
 		startActivity(intent);		
 	}
-	
-
 }
